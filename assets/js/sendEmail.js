2dc07c0f500f417dca56e4bcedf9f8aa2b256e57
Mark Dyson <52258439+MD1968@users.noreply.github.com>
2019-11-23 12:50:26 +0000

    Contadt form editied, Send/Failed /404 pages added.
    Contadt form editied, Send/Failed /404 pages added.

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,19 @@
+function sendMail(contactForm) {
+    emailjs.send("smtp_server", "silverheath", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "phone" : contactForm.phone.value,
+        "notes": contactForm.projectsummary.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+          window.open("emailSent.html", "_self");
+        },
+        function(error) {
+            console.log("FAILED", error);
+              window.open("emailFail.html", "_self");
+        }
+    );
+    return false;  // To block from loading a new page
+}
